dd14f27036fc6d5b8a0bcb9a31d37b638599c57d
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-10-01 16:48:22 -0300

    chore: atualiza arquivos de configuração do Maven e setup inicial

--- src/main/java/br/edu/ifpe/gerenciadorSalas/config/CorsConfig.java
+++ src/main/java/br/edu/ifpe/gerenciadorSalas/config/CorsConfig.java
@@ -1,17 +1,16 @@
 package br.edu.ifpe.gerenciadorSalas.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Mapeia todas as rotas que começam com /api/
-                .allowedOrigins("http://localhost:5173") // Permite solicitações desse domínio
+                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5500") // Permite solicitações dessas origens
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permite os métodos
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true); // Permite credenciais
