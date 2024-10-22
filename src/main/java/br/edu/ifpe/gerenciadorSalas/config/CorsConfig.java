672f7abe8839f103cefa6b031e521d38d24b1ac1
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-10-22 00:37:48 -0300

    chore: atualizar arquivos de configuração do Maven e setup inicial

--- src/main/java/br/edu/ifpe/gerenciadorSalas/config/CorsConfig.java
+++ src/main/java/br/edu/ifpe/gerenciadorSalas/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Mapeia todas as rotas que começam com /api/
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5500") // Permite solicitações dessas origens
+                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5500", "http://127.0.0.1:5501") // Permite solicitações dessas origens
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permite os métodos
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true); // Permite credenciais
