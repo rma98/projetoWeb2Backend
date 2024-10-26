cc2ffe101807d850caddfa2845342709f89300a6
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-10-26 17:41:04 -0300

    feat: ReservaRepository.java

--- src/main/java/br/edu/ifpe/gerenciadorSalas/repository/ReservaRepository.java
+++ src/main/java/br/edu/ifpe/gerenciadorSalas/repository/ReservaRepository.java
@@ -1,11 +1,21 @@
 package br.edu.ifpe.gerenciadorSalas.repository;
 
 import br.edu.ifpe.gerenciadorSalas.model.Reserva;
+import br.edu.ifpe.gerenciadorSalas.model.Sala;
+import br.edu.ifpe.gerenciadorSalas.model.Laboratorio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-    List<Reserva> findBySalaId(Long salaId);
-    List<Reserva> findByLaboratorioId(Long laboratorioId);
-    List<Reserva> findByUsuarioId(Long usuarioId);
+    
+    List<Reserva> findBySalaOrLaboratorioAndDataInicioBeforeAndDataFimAfter(
+            Sala sala,
+            Laboratorio laboratorio,
+            LocalDateTime dataFim,
+            LocalDateTime dataInicio
+    );
 }
