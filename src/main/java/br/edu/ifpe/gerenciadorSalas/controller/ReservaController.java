0f822ba5134248efedb32a9a6751733a61e6be32
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-10-26 17:40:32 -0300

    feat: update ReservaController.java

--- src/main/java/br/edu/ifpe/gerenciadorSalas/controller/ReservaController.java
+++ src/main/java/br/edu/ifpe/gerenciadorSalas/controller/ReservaController.java
@@ -3,12 +3,8 @@ package br.edu.ifpe.gerenciadorSalas.controller;
 import br.edu.ifpe.gerenciadorSalas.model.Reserva;
 import br.edu.ifpe.gerenciadorSalas.service.ReservaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/reservas")
@@ -17,27 +13,13 @@ public class ReservaController {
     @Autowired
     private ReservaService reservaService;
 
-    @GetMapping
-    public ResponseEntity<List<Reserva>> listarTodos() {
-        List<Reserva> reservas = reservaService.listarTodos();
-        return ResponseEntity.ok(reservas);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Reserva> buscarPorId(@PathVariable Long id) {
-        Optional<Reserva> reserva = reservaService.buscarPorId(id);
-        return reserva.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
-
     @PostMapping
-    public ResponseEntity<Reserva> salvar(@RequestBody Reserva reserva) {
-        Reserva novaReserva = reservaService.salvar(reserva);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novaReserva);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletarPorId(@PathVariable Long id) {
-        reservaService.deletarPorId(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    public ResponseEntity<String> criarReserva(@RequestBody Reserva novaReserva) {
+        try {
+            reservaService.criarReserva(novaReserva);
+            return ResponseEntity.ok("Reserva criada com sucesso!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
